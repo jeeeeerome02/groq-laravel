ba2a4db16a5fa1d2dcd22e4aa2b46a81c8c2e879
linus0221 <linus0221@trimexccs.online>
2025-07-06 01:33:45 +0800

    final fixes for ai

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChatController;
 
 // In routes/web.php
-Route::get('/chat', [ChatController::class, 'showChat']);
+Route::get('/', [ChatController::class, 'showChat']);
 Route::post('/chat/send', [ChatController::class, 'sendMessage']);
 
